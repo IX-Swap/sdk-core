2a77cb4db00a55153ea283598f43afcfc0398d11
anhthii <nguyendanganhthi247@gmail.com>
2024-12-12 11:23:37 +0700

    Add kaia and ozean testnet

--- src/entities/weth9.ts
+++ src/entities/weth9.ts
@@ -13,5 +13,8 @@ export const WETH9: { [chainId: number]: Token } = {
   [80001]: new Token(80001, '0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889', 18, 'WETH9', 'Wrapped Ether'),
   [80002]: new Token(80002, '0x0ae690AAD8663aaB12a671A6A0d74242332de85f', 18, 'WETH9', 'Wrapped Ether'),
   [84532]: new Token(84532, '0x4a2b0767ACEE85C7825F09F48A9347285F58a5C2', 18, 'WETH9', 'Wrapped Ether'),
-  [8453]: new Token(8453, '0x4200000000000000000000000000000000000006', 18, 'WETH9', 'Wrapped Ether')
+  [8453]: new Token(8453, '0x4200000000000000000000000000000000000006', 18, 'WETH9', 'Wrapped Ether'),
+  [7849306]: new Token(7849306, '0x4200000000000000000000000000000000000006', 18, 'WUSDX', 'Wrapped USDX'),
+  [8217]: new Token(8217, '0x19aac5f612f524b754ca7e7c41cbfa2e981a4432', 18, 'WKAIA', 'Wrapped Kaia(WKAIA)'),
+  [1001]: new Token(1001, '0x0511632b7280bd4c223ed0e4d86bf04fda5c7799', 18, 'WKAIA', 'Wrapped Kaia(WKAIA)')
 }
